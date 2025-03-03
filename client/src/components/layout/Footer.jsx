336aad6a1837b81177c80ca05678347c8de88549
Yashvardhan Jaiswal <136220426+Yashvardhan0718@users.noreply.github.com>
2025-03-03 16:04:21 +0530

    Update Footer.jsx

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-secondary-900 text-white pt-12 pb-6">
+    <footer className="bg-secondary-900 text-#5c5470 pt-12 pb-6">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* About */}
